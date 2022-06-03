69381b40e704be4c9c86eabdb5fc3b7780c912c1
Philipp Gloor <philipp.gloor@zhinst.com>
2022-06-03 19:08:13 +0200

    intial commit

---
+++ src/students/beat/src/person_inheritance.py
@@ -0,0 +1,52 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"I'm an abstract person which is {self.age} years old and my name is {self.name}.")
+
+class Student(Person):
+    def __init__(self, name, age, grades, lectures):
+        super().__init__(name, age)
+        self.grades = grades
+        self.lectures = lectures
+
+    def info(self):
+        print(f"I'm a stundent, my name is {self.name}, I'm {self.age} years old, my grades are {self.grades} and I study {self.lectures}.")
+
+class Teacher(Person):
+    def __init__(self, name, age, salary, lectures):
+        super().__init__(name, age)
+        self.salary = salary
+        self.lectures = lectures
+
+    def info(self):
+        print(f"I'm a teacher, my name is {self.name}, I'm {self.age} years old, my salary is {self.salary} and I lecture {self.lectures}.")
+
+class Lectures:
+    def __init__(self, topic, teacher, students):
+        self.topic = topic
+        self.teacher = teacher
+        self.students = students
+
+    def lecture_list_teacher(self):
+        self.lecture = {}
+        lecture[self.topic] = self.teacher
+
+    def lecture_list_student(self):
+        self.lecture = {}
+        count = 0
+        lecture[self.topic] = str(count += 1)
+
+
+list_of_person = [Student("Max", 17, 5, "Maths"), Teacher("Dr. Jekyll", 54, 100000, "Chemistry"), Student("Sophie", 19, 5.5, "Chemistry"), Teacher("Mr. Hyde", 55, 120000, "Maths")]
+for person in list_of_person:
+    person.info()
+
+# for person in list_of_person:
+#     if person of class Student:
+#         take lectures student
+
+
+
---
+++ src/students/beat/src/superheroes.py
@@ -0,0 +1,42 @@
+class Mutant:
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+    def info(self):
+        print(f"I'm an abstract mutant, my name is {self.name} and I'm {self.gender}.")
+
+    def superpower(self):
+        print("I'm abstract, I don't have any superpowers.")
+
+class Superhero(Mutant):
+    def __init__(self, name, gender, superpower):
+        super().__init__(name, gender)
+        self.superpower = superpower
+
+    def info(self):
+        print(f"I'm {self.name}, I'm a {self.gender} mutant and my power is: {self.superpower}.")
+
+    def classified(self):
+        print("Classification: Superhero")
+
+class Supervillain(Mutant):
+    def __init__(self, name, gender, crimes):
+        super().__init__(name, gender)
+        self.crimes = crimes
+
+    def info(self):
+        print(f"I'm {self.name}, I'm a {self.gender} mutant and I'm accused for: {self.crimes}.")
+
+    def classified(self):
+        print("Classification: Supervillain")
+
+
+list_of_mutant = [Superhero("Superman", "male", "Superstrength"), Superhero("Wonderwoman", "female", "Superspeed"), Supervillain("Joker", "male", "Robbery")]
+for mutant in list_of_mutant:
+    mutant.info()
+    mutant.classified()
+
+
+
+
