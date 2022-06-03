b4fb45f1b48c59eb0395309eb45e588b16529204
Philipp Gloor <philipp.gloor@zhinst.com>
2022-06-03 19:25:54 +0200

    Mutliply Complex Numbers

--- src/complex.py
+++ src/complex.py
@@ -8,6 +8,11 @@ class Complex:
         new_imaginary = self.imaginary + other.imaginary
         return Complex(new_real, new_imaginary)
 
+    def __mul__(self, other):
+        new_real = self.real * other.real - self.imaginary * other.imaginary
+        new_imaginary = self.real * other.imaginary + self.imaginary * other.real
+        return Complex(new_real, new_imaginary)
+
     def __str__(self):
         return f"{self.real} + {self.imaginary}i"
 
@@ -16,3 +21,4 @@ c1 = Complex(4, 3)
 c2 = Complex(-1, -7)
 
 print(c1 + c2)
+print(c1 * c2)
