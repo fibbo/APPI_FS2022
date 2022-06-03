0e4fe9ebc701a5924cb2e388c2009a3b2b6aa385
Philipp Gloor <philipp.gloor@zhinst.com>
2022-06-03 19:15:03 +0200

    Make lecture singular

--- src/students/beat/src/person_inheritance.py
+++ src/students/beat/src/person_inheritance.py
@@ -24,20 +24,32 @@ class Teacher(Person):
     def info(self):
         print(f"I'm a teacher, my name is {self.name}, I'm {self.age} years old, my salary is {self.salary} and I lecture {self.lectures}.")
 
-class Lectures:
+
+# Probably it's better to keep lecture in singular. Then it's easier:
+# A lecture as one teacher and many students.
+
+class Lecture:
     def __init__(self, topic, teacher, students):
         self.topic = topic
         self.teacher = teacher
         self.students = students
 
-    def lecture_list_teacher(self):
-        self.lecture = {}
-        lecture[self.topic] = self.teacher
+    def info(self):
+        print(f"I'm a lecture, my topic is {self.topic}, my teacher is {self.teacher}, and my students are {self.students}.")
+# class Lectures:
+#     def __init__(self, topic, teacher, students):
+#         self.topic = topic
+#         self.teacher = teacher
+#         self.students = students
 
-    def lecture_list_student(self):
-        self.lecture = {}
-        count = 0
-        lecture[self.topic] = str(count += 1)
+#     def lecture_list_teacher(self):
+#         self.lecture = {}
+#         lecture[self.topic] = self.teacher
+
+#     def lecture_list_student(self):
+#         self.lecture = {}
+#         count = 0
+#         lecture[self.topic] = str(count += 1)
 
 
 list_of_person = [Student("Max", 17, 5, "Maths"), Teacher("Dr. Jekyll", 54, 100000, "Chemistry"), Student("Sophie", 19, 5.5, "Chemistry"), Teacher("Mr. Hyde", 55, 120000, "Maths")]
